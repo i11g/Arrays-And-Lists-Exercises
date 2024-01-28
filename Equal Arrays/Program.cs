76ffe62ade2342868573f2c363325463e78c8863
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-28 07:19:35 +0200

    Add files via upload

--- Equal Arrays/Program.cs
+++ Equal Arrays/Program.cs
@@ -1,30 +1,46 @@
-﻿int[] num1=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-int[] num2=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+﻿using System.Runtime.InteropServices;
 
-bool isIdentical = true;
-
-if (num1.Length != num2.Length)
+namespace Equal_Arrays
 {
-    isIdentical = false;
-}
-
-else
-{
-    for (int i = 0; i < num1.Length; i++)
+    internal class Program
     {
-        if (num1[i] != num2[i])
+        static void Main(string[] args)
         {
-            isIdentical = false;
-            break;
-        }
+            int[] numbers=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+             int[] items=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
+            int sum = 0;
+            int differenceIndex = 0;
+            bool isEqual = true;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j <=i ; j++)
+                {
+                    if (numbers[i] == items[j])
+                    {
+                        sum += numbers[i];
+                    }
+                    else
+                    {
+                        isEqual=false;
+                        differenceIndex = i;
+                        break;
+                    }
+                }
+                if (!isEqual)
+                {
+                    break;
+                }
+            }
+            if (!isEqual)
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {differenceIndex}");
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+
+        }
     }
-}
-if(!isIdentical)
-{
-    Console.WriteLine($"Arrays are not identical.");
-}
-else  
-{
-    Console.WriteLine($"Arrays are identical.");
 }
