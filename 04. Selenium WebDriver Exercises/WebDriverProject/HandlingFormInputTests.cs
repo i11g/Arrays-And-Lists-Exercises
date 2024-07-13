26e1784d4b02e91450b51fc03fc2bf71523e43bf
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-13 16:26:15 +0300

    files added

---
+++ 04. Selenium WebDriver Exercises/WebDriverProject/HandlingFormInputTests.cs
@@ -0,0 +1,21 @@
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriverProject
+{
+    public class HandlingFormInputTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
