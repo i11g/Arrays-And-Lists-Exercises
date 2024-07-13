4b34a68540b058d8ae2b217157fd86f5c0f1e77c
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-13 13:20:46 +0300

    files changed

--- 03. Selenium WebDriver/Exercise 1/Program.cs
+++ 03. Selenium WebDriver/Exercise 1/Program.cs
@@ -16,17 +16,26 @@ namespace Exercise_1
 
             //Find element
 
-            var searchInput = driver.FindElement(By.Id("searchInput")); 
+            //var searchInput = driver.FindElement(By.Id("searchInput")); 
+
             //Click on the element
 
-            searchInput.Click();
-            Console.WriteLine(searchInput.GetCssValue("background-color"));
-            Console.WriteLine(searchInput.GetDomProperty("size"));
+            //searchInput.Click();
+            // Console.WriteLine(searchInput.GetCssValue("background-color"));
+            //Console.WriteLine(searchInput.GetDomProperty("size"));
 
 
             //Type Quality Assurance
 
-            searchInput.SendKeys("Quality Assurance" + Keys.Enter);
+            //searchInput.SendKeys("Quality Assurance" + Keys.Enter);
+
+            //Find element by cSSlocator
+
+            //driver.FindElement(By.CssSelector("#js-link-box-en")).Click();
+            //driver.FindElement(By.LinkText("Alan Wace"));
+
+            driver.FindElement(By.XPath("//*[@id='js-link-box-es']/strong")).Click();
+            driver.FindElement(By.XPath("//*[@id='Primer_franquismo']/a")).Click();
 
             //Find pageTitle
             var pageTitle = driver.Title;
