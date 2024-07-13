12207f64baec916960742be6dc5d8dc027e8be33
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-13 09:33:13 +0300

    files changed

---
+++ 03. Selenium WebDriver/Wikipedia Tests/Wikipedia.cs
@@ -0,0 +1,47 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Wikipedia_Tests
+{
+    public class Wikipedia
+    {   
+        private ChromeDriver driver;
+
+        [SetUp]
+        public void Setup()
+        { 
+            this.driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+        } 
+
+        [TearDown] 
+
+        public void TearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        [Test]
+        public void TestTitle ()
+        {
+            
+            Assert.That(driver.Title, Is.EqualTo("Wikipedia"));
+            
+        }
+
+        [Test] 
+
+        public void TestQualityAssurance()
+        {
+            
+                      
+            driver.FindElement(By.Id("searchInput")).SendKeys("Quality Assurance" + Keys.Enter);
+
+            Assert.That(driver.Title, Is.EqualTo("Quality assurance - Wikipedia"));
+
+            
+        } 
+    }
+}
