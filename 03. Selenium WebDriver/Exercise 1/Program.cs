32df1e26503bdfb93808141b7f54319e441623d2
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-13 07:35:59 +0300

    files changed

---
+++ 03. Selenium WebDriver/Exercise 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebDriver driver = new ChromeDriver();
+
+            //WebDriver driver = new FirefoxDriver();
+
+            driver.Url = "https://www.wikipedia.org/";
+
+            //Find element
+
+            var searchInput = driver.FindElement(By.Id("searchInput")); 
+            //Click on the element
+
+            searchInput.Click();
+
+            //Type Quality Assurance
+
+            searchInput.SendKeys("Quality Assurance" + Keys.Enter);
+
+            //Find pageTitle
+            var pageTitle = driver.Title;
+
+            Console.WriteLine(pageTitle); 
+
+
+        }
+    }
+}
