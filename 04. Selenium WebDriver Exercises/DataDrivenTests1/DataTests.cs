2ab8c8d9fc278315140bb552b1e4eb2cb041c93f
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-14 15:27:42 +0300

    files added

---
+++ 04. Selenium WebDriver Exercises/DataDrivenTests1/DataTests.cs
@@ -0,0 +1,16 @@
+namespace DataDrivenTests1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
