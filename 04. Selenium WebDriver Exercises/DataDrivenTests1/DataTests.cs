c0181fb9419a3c3eaf87c09aaab6d49cb0a88c71
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-14 16:10:57 +0300

    files added

--- 04. Selenium WebDriver Exercises/DataDrivenTests1/DataTests.cs
+++ 04. Selenium WebDriver Exercises/DataDrivenTests1/DataTests.cs
@@ -1,16 +1,30 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace DataDrivenTests1
 {
-    public class Tests
-    {
+    public class DataTests
+    {   
+        private WebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            this.driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com/number-calculator/");
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(15);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
         }
 
         [Test]
-        public void Test1()
+        public void DataTest()
         {
-            Assert.Pass();
+           
         }
     }
 }
