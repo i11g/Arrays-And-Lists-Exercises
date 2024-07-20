06c3799ac6eb0c69bb355d5b87d4c213435044b8
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-20 15:31:10 +0300

    files added

--- 08. Selenium WebDriver POM Exercise/POMExercise/Tests/BasePageTests.cs
+++ 08. Selenium WebDriver POM Exercise/POMExercise/Tests/BasePageTests.cs
@@ -1,31 +1,52 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using POMExercise.Pages;
+
 
 namespace POMExercise.Tests
 {
     public class BasePageTests
     {
         protected IWebDriver driver;
+
+        protected LoginPage loginPage; 
+
+        protected InventoryPage inventoryPage;
         
         [SetUp]
         
         public void SetUp()
         {
-            driver = new ChromeDriver();
+            var chromeOpnions = new ChromeOptions();
+            chromeOpnions.AddUserProfilePreference("profile.password_manager_enabled", false);
+            driver = new ChromeDriver(chromeOpnions);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            loginPage = new LoginPage(driver);
+            inventoryPage = new InventoryPage(driver);
+            
+            
+
         }
 
         [TearDown]
 
         public void TearDown() 
-        {
-            driver.Quit();
-            driver.Dispose();
+        {   
+            if(driver!=null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }           
 
+        }
+
+        protected void LoginMethod(string username, string password)
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+            loginPage.LoginUser(username, password);           
+            
         }
 
         
