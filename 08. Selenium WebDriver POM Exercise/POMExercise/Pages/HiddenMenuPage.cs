994e8896c6055158b453a2496f3a81f993190054
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-20 12:52:31 +0300

    files changed

--- 08. Selenium WebDriver POM Exercise/POMExercise/Pages/HiddenMenuPage.cs
+++ 08. Selenium WebDriver POM Exercise/POMExercise/Pages/HiddenMenuPage.cs
@@ -5,9 +5,26 @@ namespace POMExercise.Pages
 {
     public class HiddenMenuPage : BasePage 
     {
+        private readonly By hamburgerButton = By.XPath("//button[@id='react-burger-menu-btn']");
+        private readonly By logoutButton = By.XPath("//a[@id='logout_sidebar_link']");
         public HiddenMenuPage(IWebDriver driver) : base(driver)
         {
             
+        }
+
+        public void ClickHamburgerMenu()
+        {
+            Click(hamburgerButton);
+        }
+
+        public void ClickLogoutButton()
+        {
+            Click(logoutButton);
+        }
+
+        public bool IsMenuOpen()
+        {
+            return FindElement(logoutButton).Displayed;
         }
     }
 }
