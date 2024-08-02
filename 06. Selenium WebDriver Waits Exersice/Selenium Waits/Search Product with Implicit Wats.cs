82173c5c965b144c86aecdd794aea5bcc12359e0
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-08-02 16:03:38 +0300

    files added

--- 06. Selenium WebDriver Waits Exersice/Selenium Waits/Search Product with Implicit Wats.cs
+++ 06. Selenium WebDriver Waits Exersice/Selenium Waits/Search Product with Implicit Wats.cs
@@ -1,16 +1,45 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace Selenium_Waits
 {
-    public class Tests
+    public class SearchProductWithImplicitWaits  
     {
+        IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            this.driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://practice.bpbonline.com");
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(10);
+        }
+
+        [TearDown] 
+        public void TearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            driver.FindElement(By.XPath("//input[@name='keywords']")).SendKeys("keyboard");
+            driver.FindElement(By.XPath("//input[@title=' Quick Find ']")).Click();
+
+            try
+            {
+                driver.FindElement(By.XPath("//td[@align='center']//span//span[2]")).Click();
+                var text = driver.FindElement(By.XPath("//td[2]/a[1]/strong"));
+
+                Assert.That(driver.PageSource.Contains("keyboard"), "The product keyboard was not found");
+                Assert.That(text.Displayed, "The text is not displayed");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Unexpected exception: " + ex.Message);
+            }
         }
     }
 }
