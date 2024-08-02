077372ae1a03ae275daeff30239068fe3212a001
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-08-02 15:26:42 +0300

    files added

---
+++ 06. Selenium WebDriver Waits Exersice/Selenium Waits/Search Product with Implicit Wats.cs
@@ -0,0 +1,16 @@
+namespace Selenium_Waits
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
