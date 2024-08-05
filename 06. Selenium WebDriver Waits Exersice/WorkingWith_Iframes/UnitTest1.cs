ce90ef18cff2c536e6a2d85a7d30b0aee3e77e13
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-08-05 11:59:28 +0300

    files added

---
+++ 06. Selenium WebDriver Waits Exersice/WorkingWith_Iframes/UnitTest1.cs
@@ -0,0 +1,19 @@
+using OpenQA.Selenium;
+
+namespace WorkingWith_Iframes
+{
+    public class WorkingWithIFramesTests
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
