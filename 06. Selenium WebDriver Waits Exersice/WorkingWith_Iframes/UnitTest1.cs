e0f76d101257e80f6c50f209a5d1d8a752e03eff
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-08-05 15:33:32 +0300

    files changed

--- 06. Selenium WebDriver Waits Exersice/WorkingWith_Iframes/UnitTest1.cs
+++ 06. Selenium WebDriver Waits Exersice/WorkingWith_Iframes/UnitTest1.cs
@@ -1,4 +1,5 @@
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace WorkingWith_Iframes
 {
@@ -8,6 +9,8 @@ namespace WorkingWith_Iframes
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+
         }
 
         [Test]
