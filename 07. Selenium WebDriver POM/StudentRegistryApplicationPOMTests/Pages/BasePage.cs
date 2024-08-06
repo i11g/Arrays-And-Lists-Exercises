1df46c4626eab53ee25d9c82302f747b72d81b90
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-08-06 07:14:20 +0300

    files deleted

--- 07. Selenium WebDriver POM/StudentRegistryApplicationPOMTests/Pages/BasePage.cs
+++ 07. Selenium WebDriver POM/StudentRegistryApplicationPOMTests/Pages/BasePage.cs
@@ -5,7 +5,21 @@ namespace StudentRegistryApplicationPOMTests.Pages
 {
     public class BasePage
     {
-        IWebDriver driver;
+        private readonly IWebDriver driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver= driver;
+            driver.Manage().Timeouts().ImplicitWait= TimeSpan.FromSeconds(10);
+        }
+
+        public virtual string PageUrl { get; }
+        public IWebElement linkHomePage => driver.FindElement(By.XPath("//body//a[1]"));
+
+        //driver.FindElement(By.LinkText("Home")
+
+        public IWebElement linkViewStudents => driver.FindElement(By.LinkText("View Students"));
+
 
     }
 }
