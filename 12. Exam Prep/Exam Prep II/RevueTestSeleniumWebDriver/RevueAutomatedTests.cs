8d2ec8b9f4a527596cd5ccbfdc9541f28b3b9cae
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-08-11 09:00:53 +0300

    files added

--- 12. Exam Prep/Exam Prep II/RevueTestSeleniumWebDriver/RevueAutomatedTests.cs
+++ 12. Exam Prep/Exam Prep II/RevueTestSeleniumWebDriver/RevueAutomatedTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 
 namespace RevueTestSeleniumWebDriver
 {
@@ -8,7 +9,10 @@ namespace RevueTestSeleniumWebDriver
     public class RevueAutomatedTests
     {
         private WebDriver driver;
-        private static readonly string baseURL = "https://d3s5nxhwblsjbi.cloudfront.net";
+        private static readonly string baseURL = "https://d3s5nxhwblsjbi.cloudfront.net"; 
+        private readonly string eMail = "iv@test.com";
+        private readonly string pass = "123456";
+
 
         [OneTimeSetUp]
         public void OneTimeSetup()
@@ -17,7 +21,19 @@ namespace RevueTestSeleniumWebDriver
             options.AddUserProfilePreference("profile.password_manager_enabled", false);
             driver = new ChromeDriver(options);
             driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);            
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.LinkText("Login")).Click();
+
+            var emailFiled=driver.FindElement(By.XPath("//input[@id='form3Example3']"));
+
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(emailFiled).Click().Perform();
+            emailFiled.SendKeys(eMail);
+            driver.FindElement(By.XPath("//input[@id='form3Example4']")).SendKeys(pass);
+            driver.FindElement(By.XPath("//button[@class='btn btn-primary btn-block mb-4']")).Click();
+
         }
 
         [OneTimeTearDown] 
@@ -31,7 +47,7 @@ namespace RevueTestSeleniumWebDriver
         [Test, Order(1)]
         public void Create_Revue_Invalid_Data_Test()
         {
-            driver.Navigate().GoToUrl(baseURL + "/Revue/Create");
+            //driver.Navigate().GoToUrl(baseURL+"/Revue/Create");
         }
     }
 }
